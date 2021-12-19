3fee3d415f89a1504db3c4203126270a02bf5d58
ByeongJunHan <appoung@naver.com>
2021-12-19 16:55:50 +0900

    change system update
    add system be +~~ to just change to input.
    And, check all ramen info if 5seconds ago(loop)

--- add.py
+++ add.py
@@ -4,18 +4,12 @@ def ramen_add(name_input,num_input,type_input):
     c = conn.cursor()
     c.execute("CREATE TABLE IF NOT EXISTS table1 \
         (name text, num interger, type text)")
-    c.execute("SELECT num FROM table1 WHERE name=:Name", {"Name": name_input})
+    c.execute("SELECT num FROM table1 WHERE name=:Name AND type=:Type", {"Name": name_input,"Type": type_input})
     double = c.fetchone()
-    c.execute("SELECT num FROM table1 WHERE type=:Type", {"Type": type_input})
     double_type = c.fetchone()
     if double == None:
         c.execute("insert into table1 (name, num,type) values (?, ?,?)",(name_input,num_input,type_input))
-    if double_type == None:
-        c.execute("insert into table1 (name, num,type) values (?, ?,?)",(name_input,num_input,type_input))
           
     else:
-        double = double[0]
-        num1 = double + int(num_input)
-        c.execute("UPDATE table1 set num=? WHERE name=?",(num1,name_input))
-ramen_add("sin",7,"bongzi")
+        c.execute("UPDATE table1 set num=? WHERE name=?",(int(num_input),name_input))
         
--- check.py
+++ check.py
@@ -1,21 +1,29 @@
 import sqlite3
+import threading
 
-conn = sqlite3.connect("ramen.sqlite", isolation_level=None)
+def check_all():
+  conn = sqlite3.connect("ramen.sqlite", isolation_level=None)
   #get  커서 
-c = conn.cursor()
-  # 테이블 생성 (데이터 타입은 TEST, NUMERIC, INTEGER, REAL, BLOB 등)
-c.execute("CREATE TABLE IF NOT EXISTS table1 \
-(name text, num interger, type text)")
-c.execute("SELECT * FROM table1")
-all_ramen = c.fetchall()
-print(all_ramen)
-for i in range(len(all_ramen)):
-  all_name = all_ramen[i][0]
-  all_num = all_ramen[i][1]
-  all_type = all_ramen[i][2]
-  print(all_name)
-  print(all_num)
-  print(all_type)
+  c = conn.cursor()
+    # 테이블 생성 (데이터 타입은 TEST, NUMERIC, INTEGER, REAL, BLOB 등)
+  c.execute("CREATE TABLE IF NOT EXISTS table1 \
+  (name text, num interger, type text)")
+  c.execute("SELECT * FROM table1")
+  global all_ramen
+  global all_name
+  global all_num
+  global all_type
+  all_ramen = c.fetchall()
+  print(all_ramen)
+  for i in range(len(all_ramen)):
+    all_name = all_ramen[i][0]
+    all_num = all_ramen[i][1]
+    all_type = all_ramen[i][2]
+    print(all_name)
+    print(all_num)
+    print(all_type)
+  threading.Timer(3, check_all).start()
+check_all()
 def ramen_check(name_input,type_input):
     c.execute("SELECT num FROM table1 WHERE name=:Name AND type=:Type", {"Name": name_input,"Type":type_input})
     double = c.fetchone()
